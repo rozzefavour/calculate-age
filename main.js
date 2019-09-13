7d0e8dcc29727c5da133388cb262b3f8e601d40d
rozzefavour <rozzefavour@gmail.com>
2019-09-13 18:11:16 +0300

    edited some thing

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var dt = new Date();
 var currentYear = dt.getFullYear();
 var birthyear= prompt('enter year of birth');
-
 var age = currentYear - birthyear;
 
 alert("you are" + age + "years old");
