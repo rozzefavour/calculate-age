363cc42d10020ec2c7aa0536ab56c013c6441de4
rozzefavour <rozzefavour@gmail.com>
2019-09-13 18:02:44 +0300

    added age functionality

---
+++ main.js
@@ -0,0 +1,7 @@
+var dt = new Date();
+var currentYear = dt.getFullYear();
+var birthyear= prompt('enter year of birth');
+
+var age = currentYear - birthyear;
+
+alert("you are" + age + "years old");
